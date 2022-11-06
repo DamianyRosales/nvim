7ddaefdc12018bd67d092f685a5d6969b845cff1
DamianyRosales <damianyrosales@gmail.com>
2022-11-06 22:26:58 +0000

    sets config dir

--- init.lua
+++ init.lua
@@ -1,34 +1,5 @@
+require('config')
 vim.cmd([[
-    
-    syntax on
-
-    set exrc
-    set guicursor=
-    set relativenumber
-    set nohlsearch
-    set hidden
-    set noerrorbells
-    set tabstop=4
-    set softtabstop=4
-    set shiftwidth=4
-    set expandtab
-    set smartindent
-    set number
-    set nowrap
-    set noswapfile
-    set nobackup
-    set undodir=~/.config/nvim/undodir
-    set undofile
-    set incsearch
-    set scrolloff=8
-    set completeopt=menuone,noinsert,noselect
-    set colorcolumn=80
-    set signcolumn=yes
-    
-    set formatoptions-=cro
-    
-    set encoding=UTF-8
-
     if has('termguicolors')
         set termguicolors
     endif
